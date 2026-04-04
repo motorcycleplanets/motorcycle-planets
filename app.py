59590b37574617b0dc26f4e9bb12c760fd8011e4
motorcycleplanets <motorcycleplanetss.a@gmail.com>
2026-04-04 16:39:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 
 from flask import Flask
+import os
 
 app = Flask(_ _name_ _)
 
 @app.route('/')
 def home():
-    return """
-    <h1>Motorcycle Planets V14 PRO</h1>
-    <p>Sistema funcionando correctamente 🚀</p>
-    """
+    return "<h1>Motorcycle Planets V14 PRO</h1><p>Sistema funcionando correctamente</p>"
 
-if _ _name_ _ == '_ _main_ _':
-    app.run()
+if _ _name_ _ == "_ _main_ _":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
