8de78cc75782dab209d1f7ef09cc95fc207b088a
motorcycleplanets <motorcycleplanetss.a@gmail.com>
2026-04-05 14:11:58 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,20 +2,18 @@ from app import db
 
 class Cliente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    telefono = db.Column(db.String(20), nullable=False)
+    nombre = db.Column(db.String(100))
+    telefono = db.Column(db.String(20))
 
 class Moto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    marca = db.Column(db.String(100), nullable=False)
-    modelo = db.Column(db.String(100), nullable=False)
-    placa = db.Column(db.String(50), nullable=False)
-
-    cliente_id = db.Column(db.Integer, db.ForeignKey('cliente.id'), nullable=False)
+    marca = db.Column(db.String(50))
+    modelo = db.Column(db.String(50))
+    placa = db.Column(db.String(20))
+    cliente_id = db.Column(db.Integer)
 
 class Servicio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    descripcion = db.Column(db.String(200), nullable=False)
-    precio = db.Column(db.Float, nullable=False)
-
-    moto_id = db.Column(db.Integer, db.ForeignKey('moto.id'), nullable=False)
+    tipo = db.Column(db.String(100))
+    descripcion = db.Column(db.String(200))
+    moto_id = db.Column(db.Integer)
