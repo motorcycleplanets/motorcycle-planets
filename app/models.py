ccf1737b666eb1f292fd66ee423346d60e595b37
motorcycleplanets <motorcycleplanetss.a@gmail.com>
2026-04-05 12:47:17 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,4 +4,3 @@ class Cliente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100))
     telefono = db.Column(db.String(20))
-    moto = db.Column(db.String(100))
