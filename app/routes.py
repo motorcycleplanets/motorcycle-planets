7a97459a8f738329cbee407b843744e0a0e1f97f
motorcycleplanets <motorcycleplanetss.a@gmail.com>
2026-04-05 10:57:38 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
