e0b682187fc015cab33f3ecff65576ffb3654601
motorcycleplanets <motorcycleplanetss.a@gmail.com>
2026-04-05 10:31:04 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,5 @@
-from flask import Blueprint
+from app import app
 
-main = Blueprint('main', __name__)
-
-@main.route('/')
+@app.route('/')
 def home():
-    return "<h1>Motorcycle Planets V14 PRO</h1><p>Sistema funcionando correctamente 🚀</p>"
+    return "Motorcycle Planets PRO funcionando 🚀"
