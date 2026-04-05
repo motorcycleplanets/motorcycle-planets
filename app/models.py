1cb8cc27224d943cc6343a71231d375c4a75e59b
motorcycleplanets <motorcycleplanetss.a@gmail.com>
2026-04-05 11:06:17 -0500

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from app import db
+
+class Cliente(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100))
+    telefono = db.Column(db.String(20))
+
+class Moto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    marca = db.Column(db.String(50))
+    modelo = db.Column(db.String(50))
+    cliente_id = db.Column(db.Integer, db.ForeignKey('cliente.id'))
+
+class Servicio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    descripcion = db.Column(db.String(200))
+    moto_id = db.Column(db.Integer, db.ForeignKey('moto.id'))
