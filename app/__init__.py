99bfc600d874fdd41009af8f17e4f5ca8d1c27e9
motorcycleplanets <motorcycleplanetss.a@gmail.com>
2026-04-05 11:07:35 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
-from flask import Flask 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(_name_)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 from app import routes 
