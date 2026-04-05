533253c2377e1d0e4230cc4f044652e7aa82dbd7
motorcycleplanets <motorcycleplanetss.a@gmail.com>
2026-04-05 11:24:11 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,17 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, request, redirect
+from app.models import Cliente
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/nuevo_cliente', methods=['GET', 'POST'])
+def nuevo_cliente():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        telefono = request.form['telefono']
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
+        nuevo = Cliente(nombre=nombre, telefono=telefono)
+        db.session.add(nuevo)
+        db.session.commit()
+
+        return redirect('/dashboard')
+
+    return render_template('nuevo_cliente.html')
